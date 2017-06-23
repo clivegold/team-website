4e7d524dd77ba38538c3fff9b3a53037ed7b09cc
Theo Crithary <tc@apacit.com.au>
2017-06-23 22:36:59 +1000

    v0.1

--- Homework1.py
+++ Homework1.py
@@ -19,39 +19,37 @@ def alberto():
     
     return response
 
-@app.route('/michael')
-def michael():
+@app.route('/theo')
+def theo():
 
-    ## First thing in my function: read the text file "michael.txt"
+    ## First thing in my function: read the text file "theo.txt"
     ## then build my own page based on the contents of the file
 
-    with open("michael.txt") as f:
-        mylist = f.read().split(':')
-    f.close()
+    file = open("theo.txt")
+    filecontents = file.read()
+    file.close()
 
+    html_header = """<html><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="/static/apple-touch-icon.png">
+    <title>Pied Piper - Homework 1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    </head><body><div class="container center-block">"""
 
-    hobbies ="<ul>"
-    
-    for h in mylist[1].split(','):
-        hobbies += "<li>" + h + "</li>"
-    hobbies += "</ul>"
-            
-    dynamic = """<table style="width:50%"><tr><th>Name</th>"""
-    dynamic += "<th>" + mylist[0] + "</th></tr>"
-    dynamic += "<tr><td>Hobbies" + "</td><td>" + hobbies + "</td></tr>"
-    dynamic += "<tr><td>Ideal Holiday" + "</td><td>" + mylist[2] + "</td></tr>"
-    
-    response = """<html><body>
-                <style>
-                    table, th, td {
-                        border: 1px solid black;
-                        border-collapse: collapse;
-                    }
-                    th {
-                        text-align: left;
-                    }
-                </style>
-    <h1>Michael's page<br/>""" + dynamic + """</html>"""
+    html_body = """<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">Theo's Page</h3></div><div class="panel-body">"""
+    filesplit = filecontents.split(':')
+    html_body += """<table class="table table-bordered table-striped"><colgroup><col class="col-xs-1"><col class="col-xs-7"></colgroup>"""
+    html_body += """<tr><th>Name:</th><td>{}</td></tr>""".format(filesplit[0])
+    html_body += """<tr><th>Hobbies:</th><td>"""
+    for item in filesplit[1].split(','):
+        html_body += """<li>{}</li>""".format(item)
+    html_body += """</td><tr>"""
+    html_body += """<tr><th>Ideal Holiday:</th><td>{}</td></tr></table></div></div>""".format(filesplit[2])
+
+    html_footer = """</div></body></html>"""
+
+    response = html_header + html_body + html_footer
     
     return response
 
