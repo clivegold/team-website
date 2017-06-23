bc248cdf47919be1f3fc63f00103d43063ee52e5
Michael Akayan <michael@akayan.com>
2017-06-23 06:09:47 +0100

    Add files via upload

--- Homework1.py
+++ Homework1.py
@@ -9,13 +9,39 @@ def mainmenu():
     ## I need to edit "main.html" so that there is link to my function "alberto"
     return render_template('main.html')
 
-@app.route('/alberto')
-def alberto():
+@app.route('/michael')
+def michael():
 
-    ## First thing in my function: read the text file "alberto.txt"
+    ## First thing in my function: read the text file "michael.txt"
     ## then build my own page based on the contents of the file
+
+    with open("michael.txt") as f:
+        mylist = f.read().split(':')
+    f.close()
+
+
+    hobbies ="<ul>"
+    
+    for h in mylist[1].split(','):
+        hobbies += "<li>" + h + "</li>"
+    hobbies += "</ul>"
+            
+    dynamic = """<table style="width:50%"><tr><th>Name</th>"""
+    dynamic += "<th>" + mylist[0] + "</th></tr>"
+    dynamic += "<tr><td>Hobbies" + "</td><td>" + hobbies + "</td></tr>"
+    dynamic += "<tr><td>Ideal Holiday" + "</td><td>" + mylist[2] + "</td></tr>"
+    
     response = """<html><body>
-    <h1>Alberto's page<br/><h3>Add your dynamic content here</html>"""
+                <style>
+                    table, th, td {
+                        border: 1px solid black;
+                        border-collapse: collapse;
+                    }
+                    th {
+                        text-align: left;
+                    }
+                </style>
+    <h1>Michael's page<br/>""" + dynamic + """</html>"""
     
     return response
 
