0f9ca9daefc53be71e56d1b0c6f8c3562b107c86
drendums <warren.powerjones@dell.com>
2017-07-02 06:22:39 +1000

    warrens update

--- Homework1.py
+++ Homework1.py
@@ -146,6 +146,58 @@ def elliot():
     Ideal Holiday: {}</html>""".format(me_info[0], me_info[1], me_info[2])
     
     return response
+
+
+@app.route('/Warren')
+def wpj():
+
+    start_page = """<HTML><HEAD></HEAD><BODY><body bgcolor="#777799"><center><h1><font color="white"><p><a href=/>Back to Member List</a></p></center>"""
+    end_page = "</body>"
+    mid_page = """<font color="white">
+    <style>
+    table, th, td {
+       border: 1px solid white;
+       color: white;
+    }
+    </style>"""
+    strings = []
+    hobbylist = ""
+
+    filename = 'warren.txt'
+    with open(filename) as file_object:
+        lines = file_object.readlines()
+        for line in lines:
+            strings.append(line)
+        file_object.close()
+ 
+        for string in strings:
+            person = string.split(":")
+            hobbies = person[1].split(",")
+            for hobby in hobbies:
+                hobbylist = hobbylist + "<li>{}</li>".format(hobby)
+
+            mid_page = mid_page + """
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td>{}</td>
+                    </tr>
+                    <tr>
+                        <td>Hobbies</td>
+                        <td><ul>
+                            {}
+                            </ul></td>
+                    </tr>
+                    <tr>
+                        <td>Ideal Holiday</td>
+                        <td>{}</td>
+                    </tr>
+                </table>
+
+            """.format(person[0],hobbylist,person[2])
+    
+    full_page = start_page + mid_page + end_page
+    return full_page
     
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
