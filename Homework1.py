fb63e4e84b8081c0bc216fc6ad558a56576975d5
clivegold <clivegold@optusnet.com.au>
2017-07-21 15:14:52 +1000

    Clive's initial push

--- Homework1.py
+++ Homework1.py
@@ -199,5 +199,26 @@ def wpj():
     full_page = start_page + mid_page + end_page
     return full_page
     
+@app.route('/clive')
+def clive():
+
+    f = open('Clive.txt')
+    about_me = f.readlines()
+    f.close()
+    
+    me_info = []
+    for sections in about_me:
+        me_info = sections.split(':')
+
+    ## First thing in my function: read the text file "alberto.txt"
+    ## then build my own page based on the contents of the file
+    response = """<html><body>
+    <h1>Clive's page</h1></br>
+    <h3>Name: {}</br>
+    Hobbies: {}</br>
+    Ideal Holiday: {}</html>""".format(me_info[0], me_info[1], me_info[2])
+    
+    return response
+	
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
